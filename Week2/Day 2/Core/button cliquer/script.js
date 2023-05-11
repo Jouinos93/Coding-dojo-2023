45c2816e65ea2f924ae6c7f95ba354d8a50c125f
Jouinos93 <jouinimariem05@gmail.com>
2023-05-11 02:17:18 +0200

    button cliker80%

---
+++ Week2/Day 2/Core/button cliquer/script.js
@@ -0,0 +1,17 @@
+var logout=document.querySelector(".btn1")
+
+var hide=document.querySelector(".btn4")
+
+function changetext()
+{
+    logout.innerHTML="logout"
+}
+function deletesomething(){
+    hide.remove()
+}
+
+function alertmessage()
+{
+
+alert ("ninja was liked")
+}
---
+++ Week2/Day 3/Practice/dojo wheather/script.js
@@ -0,0 +1,8 @@
+function loading(){
+    alert ("loading weather report...")
+}
+var cookie=document.querySelector(".cookie-policy")
+function accept(){
+    cookie.remove()
+
+}
