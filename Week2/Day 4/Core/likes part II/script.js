24aed8ff8e1c5c15079d52b6ce9ba728e81ca6dd
Jouinos93 <jouinimariem05@gmail.com>
2023-05-14 21:07:45 +0100

    likes partII100%

--- Week2/Day 4/Core/likes part II/script.js
+++ Week2/Day 4/Core/likes part II/script.js
@@ -1,23 +1,18 @@
-var plus1=9;
-var plus2=12;
-var plus3=9;
-var likes=document.querySelector("#title>h4")
-console.log(likes)
-var likes1=document.querySelector("#likes1")
-var likes2=document.querySelector("#likes2")
 
+var likes1=document.querySelector(".likes1")
+var likes2=document.querySelector(".likes2")
+var likes3=document.querySelector(".likes3")
 
-function changelikes(){
-    plus1++;
-    likes.innerText=plus+" like(s)";
-}
 
 function changelikes1(){
-    plus2++;
-    likes1.innerText=plus1+" like(s)";
+    
+    likes1.innerHTML++;
 }
 
 function changelikes2(){
-    plus3++;
-    likes2.innerText=plus2+" like(s)";
+   likes2.innerHTML++;
+}
+
+function changelikes3(){
+   likes3.innerHTML++;
 }
