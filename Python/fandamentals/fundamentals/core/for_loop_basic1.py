89cf972bc94396127a55beccc3f6aa1d050121c5
Jouinos93 <jouinimariem05@gmail.com>
2023-05-18 13:32:07 +0100

    foor_loop_basic1

---
+++ Python/fandamentals/fundamentals/core/for_loop_basic1.py
@@ -0,0 +1,24 @@
+for i in range (0,151):
+    print (i)
+    
+for i in range (5,10001,5):
+    print (i)
+def counting_the_dojo_way():
+    
+    for i in range (1,101):
+        if i%5== 0:
+            print ("coding")
+
+        elif i%10== 0:
+            print ("coding Dojo")
+    
+def countdown_by_four():
+    for i in range (2018,0,-4):
+        print (i)
+def flexible_counter(lowNum, highNum, mult):
+    for i in range (lowNum, highNum+1):
+        if i% mult==0:
+            print (i)
+    
+    
+    
