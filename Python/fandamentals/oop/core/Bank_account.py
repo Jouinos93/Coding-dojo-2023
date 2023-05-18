a0d7353da4324b604f26956eab1016a69935d49f
Jouinos93 <jouinimariem05@gmail.com>
2023-05-18 17:11:59 +0100

    Bank_account 100%

--- Python/fandamentals/oop/core/Bank_account.py
+++ Python/fandamentals/oop/core/Bank_account.py
@@ -5,6 +5,7 @@ class BankAccount:
 
     def deposit(self, amount):
         self.balance += amount
+        return self
 
     def withdraw(self, amount):
         if self.balance >= amount:
@@ -12,35 +13,31 @@ class BankAccount:
         else:
             print("Insufficient funds: Charging a $5 fee")
             self.balance -= 5
+            return self
 
     def display_account_info(self):
         print("Balance: $" + str(self.balance))
+        return self
 
     def yield_interest(self):
-        if self.balance >= 0:
-            self.balance += self.balance * self.interest_rate
+        if self.balance > 0:
+            self.balance += (self.balance * self.interest_rate)
+            return self
 
 # Create 2 accounts
-account1 = BankAccount()
-account2 = BankAccount()
 
-# Account 1 transactions
-account1.deposit()
-account1.deposit()
-account1.deposit()
-account1.withdraw()
-account1.yield_interest()
-account1.display_account_info()  
-
-# Account 2 transactions
-account2.deposit()
-account2.deposit()
-account2.withdraw()
-account2.withdraw()
-account2.withdraw()
-account2.withdraw()
-account2.yield_interest()
-account2.display_account_info()
+ Account 1 
+account1=BankAccount(0.02,500)
+account1.deposit(100).deposit(10).deposit(20).display_account_info()
+account1.withdraw(10).display_account_info()
+account1.yield_interest(10).display_account_info()
 
 
-#manque les fonctions
+#Account 2 
+
+account2=BankAccount(0.02,900)
+account2.deposit(200).deposit(100).display_account_info()
+account2.withdraw(500).withdraw(50).withdraw(20).withdraw(10).display_account_info()
+account2.yield_interest(10).display_account_info()
+
+
--- Python/fandamentals/oop/core/Users with bankaccount.py
+++ Python/fandamentals/oop/core/Users with bankaccount.py
@@ -1,33 +1,10 @@
-class BankAccount:
-    def init(self, int_rate, balance):
-        self.int_rate = int_rate
-        self.balance = balance
-
-    def deposit(self, amount):
-        self.balance += amount
-        return self
-
-    def withdraw(self, amount):
-        if self.balance >= amount:
-            self.balance -= amount
-        elif self.balance < amount:
-            print("Insufficient funds: Charging a $5 fee")
-            self.balance -= 5
-        return self
-
-    def display_account_info(self):
-        return(f"Balance: ${self.balance}")
-
-    def yield_interest(self):
-        self.balance = self.balance * self.int_rate + self.balance
-        return self
-
+from Bank_account import BankAccount
 class User:
     def init(self,name,email):
         self.name = name
         self.email = email
         self.account = BankAccount (int_rate = 0.02, balance = 0)
-        # self.account2 = BankAccount ("savings",int_rate = 0.02, balance = 0) would use this to add separate account
+
 
     def transfer_money(self, amount, other_user):
         self.account.balance = self.account.balance - amount
