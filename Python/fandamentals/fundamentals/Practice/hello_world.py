c0ec0f754a51d63598a4f732655d0a293bbe220e
Jouinos93 <jouinimariem05@gmail.com>
2023-05-20 22:20:44 +0100

    hello_world

---
+++ Python/fandamentals/fundamentals/Practice/hello_world.py
@@ -0,0 +1,10 @@
+print("Hello World")
+name = "Mariem"
+print("Hello" ",", name) # with a comma
+print("Hello " "+", name) 
+var=9
+print("Hello " + str(var)) 
+fave_food1 = "couscous"
+fave_food2 = "pizza"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2)) # with .format()
+print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
