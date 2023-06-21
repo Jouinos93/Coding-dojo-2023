3063fee53b9aeeadc4fd5e1ca17ef978aad5bf78
Jouinos93 <jouinimariem05@gmail.com>
2023-06-21 17:06:12 +0100

    First_JSX

---
+++ MERN/React/Core/my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  
+<h1>Hello Dojo!</h1>
+<h2>Things I need to do:</h2>
+<p>*Learn React</p>
+<p>*Climb Mt.Everest</p>
+<p>*Run a marathon</p>
+<p>*Feed the dog</p>
+    
+    </div>
+  );
+}
+
+export default App;
