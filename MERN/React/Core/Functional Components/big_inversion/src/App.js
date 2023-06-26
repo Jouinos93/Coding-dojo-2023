daed258529d733d51a79468c0e787b2de74cf07f
Jouinos93 <jouinimariem05@gmail.com>
2023-06-26 15:38:22 +0100

    Big_Inversion

---
+++ MERN/React/Core/Functional Components/big_inversion/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import PersonCard from "./components/PersonCard";
+const data=[{
+  firstName:'Doe',
+  lastName:'Jane',
+  age:45,
+  haircolor:'Black'
+},
+{
+  firstName:'Smith',
+  lastName:'john',
+  age:88,
+  haircolor:'Brown'
+},
+{  firstName:'Fillmore',
+lastName:'Millard',
+age:50,
+haircolor:'Brown'},
+{
+  firstName:'Smith',
+  lastName:'Maria',
+  age:62,
+  haircolor:'Brown'
+}
+]
+function App() {
+  return (
+    <div className="App">
+      {data.map((person,idx)=>{
+      return <PersonCard person={person} key={idx}/>
+    })
+    } 
+    </div>
+  );
+}
+
+export default App;
